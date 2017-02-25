fc64437d4841d9e1626f49e1fb0771554d2dc772
XiaochenCui <jcnlcxc@163.com>
2017-02-25 20:24:28 +0800

    add mail client solution

--- mail-client/mail-client.py
+++ mail-client/mail-client.py
@@ -18,7 +18,7 @@ def main():
 
 
     send_mail(args.my_address, args.mail_server, args.their_address, args.message)
-    
+
 
     # success
     return 0
@@ -26,14 +26,27 @@ def main():
 
 def send_mail(my_address, mail_server, their_address, message):
 
-    # Fill in the code to talk to the mail server here. Read the file
-    # read-first.md to find out how to set up a mail server for testing. A log
-    # of a telnet session with your mail server may be (very) useful here.
+    client_socket = Socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((mail_server, 25))
 
-    raise NotImplementedError
+    sentences = ["helo localhost",
+                 "mail from: <" + my_address + ">",
+                 "rcpt to: <" + their_address + ">",
+                 "data",
+                 message + "\n.",
+                 "quit", ]
+    for s in sentences:
+        reply = client_socket.recv(2048)
+        print("reply: ", reply)
+
+        s += "\n"
+        print(s.encode("utf-8"))
+        client_socket.send(s.encode("utf-8"))
+
+    client_socket.close()
 
     return
-    
+
 
 
 if __name__ == "__main__":
